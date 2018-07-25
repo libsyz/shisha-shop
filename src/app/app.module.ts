9602ba908314e9ab284d84b1b51b917931ca764e
miguel jimenez <denkatalabeno@gmail.com>
2018-07-25 15:15:17 +0200

    built successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { UserComponent } from './user/user.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-}
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -29,6 +28,7 @@ import { HomeComponent } from './home/home.component';
     OrdersComponent,
     LoginComponent,
     AdminOrdersComponent,
+    AdminProductsComponent,
     HomeComponent,
     NotfoundComponent
   ],
@@ -38,6 +38,10 @@ import { HomeComponent } from './home/home.component';
       {
         path: "shopping-cart",
         component: ShoppingCartComponent
+      },
+      {
+        path: 'products',
+        component: ProductsComponent
       },
       {
         path: "user",
